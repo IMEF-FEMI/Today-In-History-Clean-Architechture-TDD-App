35effd544548c89484c85b7fc8a6bb2ee6f67e1a
iMEF <bolajifemi28@gmail.com>
2021-03-13 03:17:15 +0100

    --- Auto-Git Commit ---

--- lib/features/today_in_history/domain/repositories/today_in_history_repository.dart
+++ lib/features/today_in_history/domain/repositories/today_in_history_repository.dart
@@ -1,7 +1,7 @@
 
-import 'package:dartz/dartz.dart';
 import 'package:today_in_history/core/error/failures.dart';
 import 'package:today_in_history/features/today_in_history/domain/entities/today_events.dart';
+import 'package:dartz/dartz.dart;
 
 abstract class TodayInHistoryRepository {
   Future<Either<Failure, TodayEvents>> getEventsForToday();
