88420b7684c71e196302333f876c76b69a68a732
iMEF <bolajifemi28@gmail.com>
2021-03-14 00:07:18 +0100

    --- Auto-Git Commit ---

--- lib/features/today_in_history/data/repositories/today_in_history_repository_impl.dart
+++ lib/features/today_in_history/data/repositories/today_in_history_repository_impl.dart
@@ -39,7 +39,7 @@ class TodayInHistoryRepositoryImpl implements TodayInHistoryRepository {
     if (await networkInfo.isConnected) {
       try {
         final remoteEvent = await todayOrSpecificDay();
-       await localDataSource.cacheTIHEvents(remoteEvent);
+        localDataSource.cacheTIHEvents(remoteEvent);
         return Right(remoteEvent);
       } on ServerException {
         return Left(ServerFailure());
