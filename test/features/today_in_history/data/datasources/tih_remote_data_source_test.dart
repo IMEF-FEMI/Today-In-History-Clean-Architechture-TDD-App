0d75388a84a70eca8f80715f4db4805ab9b3756e
iMEF <bolajifemi28@gmail.com>
2021-03-13 23:02:16 +0100

    --- Auto-Git Commit ---

--- test/features/today_in_history/data/datasources/tih_remote_data_source_test.dart
+++ test/features/today_in_history/data/datasources/tih_remote_data_source_test.dart
@@ -106,7 +106,7 @@ void main() {
         final TodayEventsModel result = await dataSource.getEventsForToday();
 
         // assert
-        expect(result.toJson(), equals(tTodayEventsModel));
+        expect(result.toJson(), equals(tTodayEventsModel.toJson()));
       });
 
       test('Should throw ServerException when the response is not 200',
