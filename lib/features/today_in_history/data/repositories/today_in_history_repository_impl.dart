5a224a236cbb72444f75d914518b8efc1e989d15
iMEF <bolajifemi28@gmail.com>
2021-03-13 03:16:39 +0100

    --- Auto-Git Commit ---

--- lib/features/today_in_history/data/repositories/today_in_history_repository_impl.dart
+++ lib/features/today_in_history/data/repositories/today_in_history_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:today_in_history/core/error/exceptions.dart';
 import 'package:today_in_history/core/error/failures.dart';
+import 'package:today_in_history/core/network/network_info.dart';
 import 'package:today_in_history/features/today_in_history/data/datasources/datasources.dart';
 import 'package:today_in_history/features/today_in_history/domain/entities/today_events.dart';
 import 'package:today_in_history/features/today_in_history/domain/repositories/today_in_history_repository.dart';
