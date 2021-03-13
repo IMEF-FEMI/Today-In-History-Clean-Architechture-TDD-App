c019cdadc15c1408e923c520e4b7b410d27d4534
iMEF <bolajifemi28@gmail.com>
2021-03-13 22:35:39 +0100

    --- Auto-Git Commit ---

--- lib/features/today_in_history/data/models/today_events_model.dart
+++ lib/features/today_in_history/data/models/today_events_model.dart
@@ -21,7 +21,7 @@ class TodayEventsModel extends TodayEvents {
     );
   }
   factory TodayEventsModel.fromLocalJson(Map json) {
-    print(json['events'].runtimeType);
+    // print(json['events'].runtimeType);
     return TodayEventsModel(
       date: json['date'],
       url: json['url'],
