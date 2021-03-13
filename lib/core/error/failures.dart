678979764edefaad3a82fa34d688d3dd110026e4
iMEF <bolajifemi28@gmail.com>
2021-03-14 00:21:00 +0100

    --- Auto-Git Commit ---

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -6,7 +6,7 @@ abstract class Failure  extends Equatable{}
 class ServerFailure extends Failure {
   @override
   // TODO: implement props
-  List<Object> get props => [throw UnimplementedError()];
+  List<Object> get props => ["throw UnimplementedError()"];
 }
 
 class CacheFailure extends Failure {}
