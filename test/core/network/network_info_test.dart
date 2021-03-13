d8217784848320b809f22b07dcf8f3fe9e3af8d4
iMEF <bolajifemi28@gmail.com>
2021-03-13 03:16:06 +0100

    --- Auto-Git Commit ---

--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -1,4 +1,3 @@
-import 'package:clean_architecture_app/core/network/network_info.dart';
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
