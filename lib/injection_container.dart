d08b69142e820c85e67cf8558898da9a2eb718c4
iMEF <bolajifemi28@gmail.com>
2021-03-14 07:41:16 +0100

    --- Auto-Git Commit ---

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:today_in_history/features/today_in_history/presentation/bloc/today_in_history_bloc.dart';
 
+import 'features/today_in_history/domain/usecases/get_events_for_date.dart';
+
 final serviceLocator = GetIt.instance;
 
 Future<void> init() async {
