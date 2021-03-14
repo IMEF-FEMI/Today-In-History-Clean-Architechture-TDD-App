edf95c5f21ff01163848f91bce536bd401aa2be0
iMEF <bolajifemi28@gmail.com>
2021-03-14 07:39:06 +0100

    --- Auto-Git Commit ---

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -3,6 +3,14 @@ import 'package:get_it/get_it.dart';
 final serviceLocator = GetIt.instance;
 
 Future<void> init() async {
+  //! Features - Number Trivia
+  // Bloc
+  sl.registerFactory(
+    () => NumberTriviaBloc(
+      concrete: sl(),
+      inputConverter: sl(),
+      random: sl(),
+    ),
+  );
 
-  
 }
