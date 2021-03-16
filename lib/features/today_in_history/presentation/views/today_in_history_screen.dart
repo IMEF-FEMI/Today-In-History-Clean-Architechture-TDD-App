7c0784ec4084d1147eea009308330339a9046092
iMEF <bolajifemi28@gmail.com>
2021-03-16 08:03:53 +0100

    --- Auto-Git Commit ---

--- lib/features/today_in_history/presentation/views/today_in_history_screen.dart
+++ lib/features/today_in_history/presentation/views/today_in_history_screen.dart
@@ -21,7 +21,6 @@ class _TodayInHistoryViewState extends State<TodayInHistoryView> {
       today.add(Duration(days: 2)),
       today.add(Duration(days: 3)),
     ];
-    print(selectableDates);
   }
 
   @override
