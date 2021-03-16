5a24314f51559b5a209ca0d91e7ecdf296495097
iMEF <bolajifemi28@gmail.com>
2021-03-16 07:50:23 +0100

    --- Auto-Git Commit ---

--- lib/features/today_in_history/presentation/views/today_in_history_screen.dart
+++ lib/features/today_in_history/presentation/views/today_in_history_screen.dart
@@ -7,7 +7,7 @@ class TodayInHistoryView extends StatefulWidget {
 
 class _TodayInHistoryViewState extends State<TodayInHistoryView> {
   DateTime today = DateTime.now();
-  List<DateTime>
+  List<DateTime> selec
   @override
   void initState() {
     super.initState();
