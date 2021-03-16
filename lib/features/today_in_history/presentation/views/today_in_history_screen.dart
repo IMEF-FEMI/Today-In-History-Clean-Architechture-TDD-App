876c537ceb57ca335a7c355393bf0ff608202e54
iMEF <bolajifemi28@gmail.com>
2021-03-16 06:55:28 +0100

    --- Auto-Git Commit ---

--- lib/features/today_in_history/presentation/views/today_in_history_screen.dart
+++ lib/features/today_in_history/presentation/views/today_in_history_screen.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class Today extends StatefulWidget {
+  @override
+  _TodayState createState() => _TodayState();
+}
+
+class _TodayState extends State<Today> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
