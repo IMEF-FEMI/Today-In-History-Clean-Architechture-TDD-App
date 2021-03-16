0c787428852e93721c749646976fa892bb9f7d0d
iMEF <bolajifemi28@gmail.com>
2021-03-16 06:50:28 +0100

    --- Auto-Git Commit ---

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
       
-        primarySwatch: Colors.blue,
+        primarySwatch: Color(0xff),
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
