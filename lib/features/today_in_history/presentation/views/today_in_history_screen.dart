0999d5d73fe4c62929a3de8c9e258a3a85f2a224
iMEF <bolajifemi28@gmail.com>
2021-03-16 07:57:33 +0100

    --- Auto-Git Commit ---

---
+++ lib/features/today_in_history/presentation/views/today_in_history_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TodayInHistoryView extends StatefulWidget {
+  @override
+  _TodayInHistoryViewState createState() => _TodayInHistoryViewState();
+}
+
+class _TodayInHistoryViewState extends State<TodayInHistoryView> {
+  DateTime today = DateTime.now();
+  List<DateTime> selectableDates;
+
+  @override
+  void initState() {
+    super.initState();
+    selectableDates = [
+      today.subtract(Duration(days: 3)),
+      today.subtract(Duration(days: 2)),
+      today.subtract(Duration(days: 1)),
+      today,
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Today in History"),
+      ),
+    );
+  }
+}
